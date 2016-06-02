5408ca34f05a05813df040b5b5c15007cf811001
Andrew Carmer <carmerandrew@gmail.com>
2016-06-01 22:23:01 -0600

    add product - migration, model, controller, show/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
 
+  resources :products, only: [:index, :show]
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
